c48b3f1007d396fd41d5080fb549052469afb5c5
Afik Mark <amark@I-O-Tech.com>
2023-02-16 10:12:55 +0200

    Separated retrieve test details method with writing test details to properties file added docstrings and type hints

--- page_tests/base_test.py
+++ page_tests/base_test.py
@@ -8,12 +8,17 @@ from page_objects.nav_bar import NavBar
 from page_objects.results_page import ResultsPage
 from .conftest import Drivers, create_driver
 from utils.files import write
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class TestBase:
 
     @pytest.fixture(scope="session")
-    def driver(self):
+    def driver(self) -> WebDriver:
+        """
+        Creates Webdriver according to webdriver type
+        returns the driver as a fixture for all tests to use
+        """
         try:
             driver = create_driver(browser_type=Drivers.CHROME)
             if driver.name == Drivers.FIREFOX.value.lower():
@@ -23,56 +28,65 @@ class TestBase:
             print("The request has timed out")
 
     @pytest.fixture(autouse=True)
-    def setup_teardown(self, driver):
+    def setup_teardown(self, driver: WebDriver):
         assert True
         yield
         driver.quit()
 
     @pytest.fixture
-    def home_page(self, driver) -> HomePage:
+    def home_page(self, driver: WebDriver) -> HomePage:
         """
         :returns home page object
         """
         return HomePage(driver)
 
     @pytest.fixture
-    def results_page(self, driver) -> ResultsPage:
+    def results_page(self, driver: WebDriver) -> ResultsPage:
         """
         :returns home page object
         """
         return ResultsPage(driver)
 
     @pytest.fixture
-    def contact_page(self, driver) -> ContactUsPage:
+    def contact_page(self, driver: WebDriver) -> ContactUsPage:
         """
         returns contact us page
         """
         return ContactUsPage(driver)
 
     @pytest.fixture
-    def nav_bar(self, driver) -> NavBar:
+    def nav_bar(self, driver: WebDriver) -> NavBar:
         """
         :returns NavBar Page
         """
         return NavBar(driver)
 
     @pytest.fixture
-    def result_path(self):
+    def result_path(self) -> str:
         return os.getcwd() + r'\allure-results'
 
     @pytest.fixture
-    def relative_result_path(self):
+    def relative_result_path(self) -> str:
         return os.path.abspath("..//") + r'\allure-results'
 
     @pytest.fixture(autouse=True)
-    def write_test_details(self, driver, result_path, relative_result_path):
+    def test_details(self, driver: WebDriver) -> dict:
+        """
+        retrieve current driver information
+        """
+        return {
+            'name': driver.name.capitalize(),
+            'version': driver.capabilities['browserVersion'],
+            'platform': driver.capabilities['platformName']
+        }
+
+    @pytest.fixture(autouse=True)
+    def write_test_details(self, test_details: dict, result_path: str, relative_result_path: str) -> None:
+        """
+        Get current driver information and writes it to environment properties file in the allure-results path
+        """
         try:
-            details = {
-                'name': driver.name.capitalize(),
-                'version': driver.capabilities['browserVersion'],
-                'platform': driver.capabilities['platformName']
-            }
-            info = f'Browser={details["name"]}\nVersion={details["version"]}\nPlatform={details["platform"]}'
+            info = f'Browser={test_details["name"]}\nVersion={test_details["version"]}\nPlatform={test_details["platform"]}'
 
             if not result_path:
                 write(f'{relative_result_path}/environment.properties', info)
@@ -81,4 +95,3 @@ class TestBase:
                 write(f'{result_path}/environment.properties', info)
         except (FileNotFoundError, AttributeError) as e:
             print(e)
-
--- utils/files.py
+++ utils/files.py
@@ -1,3 +1,7 @@
-def write(file_path: str, info):
+def write(file_path: str, info: str) -> None:
+    """
+    get file path and the information to write in the file
+    writes the file to the input path
+    """
     with open(file_path, 'w') as f:
         f.write(info)
