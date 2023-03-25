f2b4442a58fa4084de8e30e3e4d6ac046f88e260
Afik Mark <amark@I-O-Tech.com>
2023-03-25 17:01:01 +0300

    Created select object used select object in dropdown object

--- page_objects/dropdown/dropdown.py
+++ page_objects/dropdown/dropdown.py
@@ -1,40 +1,39 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 from decorators.decorators import default_logging
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.remote.webelement import WebElement
+from ..select.select import DriverSelect
 
 
 class DropDownObj(ABC):
+    drop_down: WebElement
 
     @abstractmethod
-    def select(self, value):
+    def get_drop_down_options(self):
         """
-        This method is responsible for selecting an item from dropdown
+        This method is responsible for getting all options from dropdown
         """
+        pass
 
 
 class DropDown(DropDownObj):
-    @default_logging
-    def select(self, element: WebElement, index: int):
-        """
-        This method is responsible for selecting an item from the dropdown by index
-        """
-        selector = Select(element)
-        selector.select_by_index(index)
+    def __init__(self, drop_down_element):
+        self.drop_down = drop_down_element
+        self.driver_select = DriverSelect()
 
     @default_logging
-    def select(self, element: WebElement, value):
-        """
-        This method is responsible for selecting an item from the dropdown by value
-        """
-        selector = Select(element)
-        selector.select_by_value(value)
+    def select_index(self, index: int):
+        self.driver_select.select_index(element=self.drop_down, index=index)
 
     @default_logging
-    def select(self, element: WebElement, text: str):
-        """
-        This method is responsible for selecting an item from the dropdown by visible text
-        """
-        selector = Select(element)
-        selector.select_by_visible_text(text)
+    def select_value(self, value):
+        self.driver_select.select_value(element=self.drop_down, value=value)
 
+    @default_logging
+    def select_text(self, text: str):
+        self.driver_select.select_text(element=self.drop_down, text=text)
+
+    def get_drop_down_options(self) -> list:
+        """
+        This method is responsible for getting all options from dropdown
+        """
+        return self.driver_select.get_options()
